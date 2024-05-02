cb5380bcedc2b242a140b6384d4d21bf8f37ff1c
Faez <faez@psight.com>
2024-05-02 13:00:47 +0400

    async and multithreaded added

--- thumbnailer.py
+++ thumbnailer.py
@@ -1,19 +1,22 @@
 import asyncio
 import os
 import subprocess
+from concurrent.futures import ThreadPoolExecutor
 
 import strapi_models as M
 
+lock = asyncio.Semaphore(50)
 
-def extract_first_frame(video_url):
-    output_path = "./temp.jpg"
+
+def extract_first_frame(video_url, video_id):
+    output_path = f"./thumbnails/temp-{video_id}.jpg"
     ffmpeg_command = [
         "ffmpeg",
         "-y",
+        "-ss",
+        "00:00:50",
         "-i",
         video_url,
-        "-ss",
-        "00:00:00",
         "-frames:v",
         "1",
         output_path,
@@ -22,17 +25,37 @@ def extract_first_frame(video_url):
     return output_path
 
 
-async def main():
-    all_videos = await M.PostCourseVideo.all()
-    all_videos = [i for i in all_videos if not i.first_frame and i.video_file]
-    for video in all_videos:
+async def process_video(video, executor):
+    async with lock:
+
+        loop = asyncio.get_running_loop()
         print(video.id)
-        frame_path = extract_first_frame(video.video_file.url)
+        frame_path = await loop.run_in_executor(
+            executor, extract_first_frame, video.video_file.url, video.id
+        )
+
         thumbnail = await M.Media.upload_file(frame_path)
-        assert thumbnail is not None
         updates = {"first_frame": thumbnail}
+        print("UPDATE", video.id)
         await M.PostCourseVideo(**{**video.__dict__, **updates}).put()
         os.remove(frame_path)
+        print(f"Processed video {video.id}")
+
+
+async def main():
+    print("Start processing videos")
+    all_videos = await M.PostCourseVideo.all()
+    all_videos = [
+        i for i in all_videos if i.first_frame and i.first_frame.name == "temp.jpg"
+    ]
+    print("ALL VIDEOS DONE")
+
+    with ThreadPoolExecutor() as executor:
+        tasks = [process_video(video, executor) for video in all_videos]
+        print("TASKS")
+        await asyncio.gather(*tasks)
+
+    print("Finished processing all videos")
 
 
 if __name__ == "__main__":
