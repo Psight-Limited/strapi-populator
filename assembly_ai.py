fb740c7278241bdbcc0280736b2b372119da3416
NoahPsight <120606151+NoahPsight@users.noreply.github.com>
2024-05-02 00:34:54 -0700

    upd

--- assembly_ai.py
+++ assembly_ai.py
@@ -1,14 +1,68 @@
+import asyncio
 import os
 
+import aiohttp
 import assemblyai as aai
 
 aai.settings.api_key = os.getenv("ASSEMBLY_AI")
-
 transcriber = aai.Transcriber()
+aai.Transcript.get_redacted_audio_url
 config = aai.TranscriptionConfig(speaker_labels=True)
+_get_all_transcripts_cache = None
+_fetch_lock = asyncio.Lock()
 
 
-def transcribe(video_url: str):
-    transcription = transcriber.transcribe(video_url, config=config)
-    paragraphs = transcription.get_paragraphs()
-    return "\n\n".join([p.text for p in paragraphs])
+async def transcribe(video_url: str):
+    try:
+        transcript = await find_video_url_in_completed(video_url)
+        if not transcript:
+            print(f"transcribing")
+            transcript = await asyncio.wrap_future(
+                transcriber.transcribe_async(video_url, config=config)
+            )
+            print(f"done transcribing")
+        paragraphs = transcript.get_paragraphs()
+        return "\n\n".join([p.text for p in paragraphs])
+    except Exception as e:
+        print(e)
+        raise
+
+
+async def find_video_url_in_completed(video_url: str):
+    for t in await get_all_transcripts():
+        if t["audio_url"] == video_url:
+            return await asyncio.wrap_future(aai.Transcript.get_by_id_async(t["id"]))
+
+
+async def get_all_transcripts():
+    async with _fetch_lock:
+        global _get_all_transcripts_cache
+        if _get_all_transcripts_cache is not None:
+            return _get_all_transcripts_cache
+
+        url = "https://api.assemblyai.com/v2/transcript?limit=200&status=completed"
+
+        async def fetch(url):
+            async with aiohttp.ClientSession() as session:
+                try:
+                    response = await session.get(
+                        url=url,
+                        headers={"Authorization": aai.settings.api_key},
+                    )
+                    data = await response.json()
+                    return data
+                except Exception as e:
+                    print(f"Failed to fetch data: {e}")
+                    return None
+
+        results = []
+        while url:
+            data = await fetch(url)
+            if data:
+                results.extend(data["transcripts"])
+                url = data["page_details"].get("prev_url")
+                print("Next URL:", url)
+            else:
+                break
+        _get_all_transcripts_cache = results
+        return results
--- audioer.py
+++ audioer.py
@@ -5,17 +5,12 @@ import subprocess
 import strapi_models as M
 
 
-def extract_first_frame(video_url):
-    output_path = "./temp.jpg"
+def extract_audio(video_url):
+    output_path = "./temp.mp3"
     ffmpeg_command = [
         "ffmpeg",
-        "-y",
         "-i",
         video_url,
-        "-ss",
-        "00:00:00",
-        "-frames:v",
-        "1",
         output_path,
     ]
     subprocess.run(ffmpeg_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -24,15 +19,15 @@ def extract_first_frame(video_url):
 
 async def main():
     all_videos = await M.PostCourseVideo.all()
-    all_videos = [i for i in all_videos if not i.first_frame and i.video_file]
+    all_videos = [i for i in all_videos if not i.audio_file and i.video_file]
     for video in all_videos:
         print(video.id)
-        frame_path = extract_first_frame(video.video_file.url)
-        thumbnail = await M.Media.upload_file(frame_path)
-        assert thumbnail is not None
-        updates = {"first_frame": thumbnail}
+        audio_path = extract_audio(video.video_file.url)
+        audio = await M.Media.upload_file(audio_path)
+        assert audio is not None
+        updates = {"audio_file": audio}
         await M.PostCourseVideo(**{**video.__dict__, **updates}).put()
-        os.remove(frame_path)
+        os.remove(audio_path)
 
 
 if __name__ == "__main__":
